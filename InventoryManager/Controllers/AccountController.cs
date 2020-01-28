484c366696e068b14424887f2e7109a3af5b123d
Lauren Girolmetti <laurengiro@gmail.com>
2020-01-28 00:04:19 -0500

    Worked on user registration and login

---
+++ InventoryManager/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using InventoryManager.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+                                 SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Register() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registerViewModel.Email, Email = registerViewModel.Email };
+                var result = await userManager.CreateAsync(user, registerViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(registerViewModel);
+        }
+    }
+}
---
+++ InventoryManager/Data/InventoryManagerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManager.Data
+{
+    public class InventoryManagerDbContext : IdentityDbContext
+    {
+        public InventoryManagerDbContext(DbContextOptions<InventoryManagerDbContext> options)
+            : base(options)
+        { }
+    }
+}
