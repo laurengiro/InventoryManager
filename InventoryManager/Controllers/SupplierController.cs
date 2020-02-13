b7dfc87af3bc61e5d5a532431d321367bf81a9ac
Lauren Girolmetti <laurengiro@gmail.com>
2020-02-12 23:10:50 -0500

    Added edit and delete functionality to suppliers

--- InventoryManager/Controllers/SupplierController.cs
+++ InventoryManager/Controllers/SupplierController.cs
@@ -51,5 +51,57 @@ namespace InventoryManager.Controllers
 
             return View(addSupplierViewModel);
         }
+
+        public IActionResult Edit(int id)
+        {
+            Supplier theSupplier = context.Suppliers.Single(s => s.ID == id);
+            EditSupplierViewModel editSupplierViewModel = new EditSupplierViewModel()
+            {
+                ID = theSupplier.ID,
+                Name = theSupplier.Name,
+                Country = theSupplier.Country
+            };
+
+            return View(editSupplierViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditSupplierViewModel editSupplierViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Supplier updateSupplier = context.Suppliers.Single(s => s.ID == editSupplierViewModel.ID);
+                updateSupplier.Name = editSupplierViewModel.Name;
+                updateSupplier.Country = editSupplierViewModel.Country;
+
+                context.Update(updateSupplier);
+                context.SaveChanges();
+
+                return Redirect("/Supplier");
+            }
+
+            return View(editSupplierViewModel);
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.title = "Remove Suppliers";
+            IList<Supplier> suppliers = context.Suppliers.ToList();
+            return View(suppliers);
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int[] supplierIds)
+        {
+            foreach (int supplierId in supplierIds)
+            {
+                Supplier theSupplier = context.Suppliers.Single(s => s.ID == supplierId);
+                context.Suppliers.Remove(theSupplier);
+            }
+
+            context.SaveChanges();
+
+            return Redirect("/Supplier");
+        }
     }
 }
---
+++ InventoryManager/ViewModels/EditSupplierViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManager.ViewModels
+{
+    public class EditSupplierViewModel : AddSupplierViewModel
+    {
+        public int ID { get; set; }
+    }
+}
